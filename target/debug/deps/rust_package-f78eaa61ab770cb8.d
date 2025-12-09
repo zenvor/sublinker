a16ae75d5bd38f065f14111c9d16397394c2b1d9
passing-dream <ybl631185@gmail.com>
2025-12-09 14:13:21 +0800

    refactor: 重构为 monorepo 结构
    - 将项目拆分为 JavaScript 和 Rust 两个子包
    - 更新 package.json 和 package-lock.json
    - 添加 monorepo 相关配置和脚本

---
+++ target/debug/deps/rust_package-f78eaa61ab770cb8.d
@@ -0,0 +1,5 @@
+/Users/ybl/Documents/github/CloakGate/target/debug/deps/rust_package-f78eaa61ab770cb8.d: packages/rust-package/src/main.rs
+
+/Users/ybl/Documents/github/CloakGate/target/debug/deps/librust_package-f78eaa61ab770cb8.rmeta: packages/rust-package/src/main.rs
+
+packages/rust-package/src/main.rs:
